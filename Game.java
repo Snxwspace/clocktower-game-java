95f4158798a238c993f51544c1f76e7458af247f
Snowspace <snxwspace@gmail.com>
2026-03-17 12:46:50 -0300

    added code to handle creating and filling the players array in Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Game {
     private Player[] players;
+
+    public Game(int numPlayers) {
+        players = new Player[numPlayers];
+    }
+
+    public void fillPlayerArray(Scanner sc) {
+        sc.nextLine();
+        System.out.println("Please answer the questions about each player going in clockwise order. It doesn't matter which player you start with.");
+        for(int i = 0; i < players.length; i++) {
+            System.out.println("Player " + i+1 + " Name: ");
+            String name = sc.nextLine();
+            // probably more things to instantiate Player objects
+            players[i] = new Player(name);
+        }
+    }
 }
