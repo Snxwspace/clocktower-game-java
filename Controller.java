f4f622dc5323d624c5daeef75cebbf7281dedeb5
Snowspace <snxwspace@gmail.com>
2026-03-17 12:33:35 -0300

    renamed Main() to Controller() and added scanner

---
+++ Controller.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Controller {
+    private Scanner sc;
+
+    public Controller() {
+        sc = new Scanner(System.in);
+    }
+
+    public static void main(String[] args) {
+        // main function, all working code goes here!
+    }
+}
