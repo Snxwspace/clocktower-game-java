90a7e9ed48b829941c9d5e1fa2f62f92b531738e
wlachance <eritfortuna@gmail.com>
2026-03-17 13:30:47 -0300

    Updated Script.java
    added a constructor

--- Script.java
+++ Script.java
@@ -6,4 +6,11 @@ public class Script {
     private ArrayList<PlayerCharacter> minions;
     private ArrayList<PlayerCharacter> demons;
     // unused as of now, we'll figure out what to do with this
+
+    public Script(ArrayList<PlayerCharacter> townsfolk, ArrayList<PlayerCharacter> outsiders, ArrayList<PlayerCharacter> minions, ArrayList<PlayerCharacter> demons){
+        this.townsfolk = townsfolk;
+        this.outsiders = outsiders;
+        this.minions = minions;
+        this.demons = demons;
+    }
 }
