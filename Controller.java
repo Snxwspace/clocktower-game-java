ab5662a1fe9a33ea0f94d778b70089bca5444846
Snowspace <snxwspace@gmail.com>
2026-03-17 23:01:16 -0300

    Added script choice framework, player count and game initialization, and slight reformatting

--- Controller.java
+++ Controller.java
@@ -1,14 +1,64 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Controller {
-    private Scanner sc;
-    private Game game;
+    // TODO scripts  --Snow
+    public static void main(String[] args) {
+        // Filling basic variables
+        Scanner sc = new Scanner(System.in);
+        Game game;
+        ArrayList<PlayerCharacter> script = null;
 
-    public Controller() {
-        sc = new Scanner(System.in);
+        // TODO fill out these messages
+        System.out.println(""); // Welcome statement
+
+        // This do-while loop will keep looping until the user chooses a script to play
+        do {
+            System.out.println("Which script would you like to play?");
+            System.out.println("1. Trouble Brewing");
+            // System.out.println("2. Bad Moon Rising");
+            // System.out.println("3. Sects and Violets");
+            // System.out.println("4. Custom Script");
+            System.out.println("Type 0 to cancel.");
+            int choice = sc.nextInt();
+            switch(choice) {
+                case 1:
+                    // TODO trouble brewing
+                    break;
+                case 0:
+                    System.out.println("Cancelling game...");
+                    return;
+                default:
+                    System.out.println("Please enter a valid choice.");
+                    break;
+            }
+        } while(script == null);
+        
+        // This while loop will keep looping until the user selects a valid number of players
+        System.out.println("How many players are playing? The minimum player count is 5, and the maximum is 15.");
+        System.out.println("Type 0 to cancel.");
+        int numPlayers = sc.nextInt();
+        while(numPlayers > 15 || numPlayers < 5) {
+            if(numPlayers == 0) {
+                System.out.println("Cancelling game...");
+                return;
+            } else if(numPlayers > 15) {
+                System.out.println("Too many players are playing!"); // add "Please consider adding Travellers." when implemented (future scalability)
+            } else {
+                System.out.println("You don't have enough players for a game!");
+            }
+            System.out.println("How many players are playing? The minimum player count is 5, and the maximum is 15.");
+            System.out.println("Type 0 to cancel.");
+            numPlayers = sc.nextInt();
+        }
+        game = new Game(numPlayers);
+
+        // TODO choosing characters that are in play (requires PlayerCharacter)
+
+        // TODO filling in the player information (requires PlayerCharacter, Player)
     }
 
-    public static void main(String[] args) {
-        // main function, all working code goes here!
+    public static ArrayList<PlayerCharacter> scriptTroubleBrewing() {
+        return null; // to satisfy the computer while I wait for PlayerCharacter to finish
     }
 }
