9e8e052f308dcb20f0c97d4e549d728646ef5f7d
Snowspace <snxwspace@gmail.com>
2026-03-24 21:04:59 -0300

    important poisoner fix

--- Poisoner.java
+++ Poisoner.java
@@ -1,10 +1,7 @@
-public class Poisoner extends PlayerCharacter {
-    private boolean canAct;
-    private boolean picksPlayer;
-    private char charType;
-    private char baseAlignment;
-    private String name;
+import java.util.Random;
+import java.util.Scanner;
 
+public class Poisoner extends PlayerCharacter {
     public Poisoner(char charType, String name) {
         super(charType, name);
         baseAlignment = 'e';
@@ -12,8 +9,21 @@ public class Poisoner extends PlayerCharacter {
         picksPlayer = true;
     }
     
-    // TODO fix that
-    public void useAbility(Player guy){
-        guy.setPoisoned(true);
+    @Override
+    public void useAbility(Scanner sc, Game game, Random rand){
+        if (canAct == true){
+            boolean poison = false;
+            do{
+                System.out.println("Who would they like to poison?");
+                String protectee = sc.nextLine();
+                for (Player player : game.getPlayers()) {
+                    if (player.getName().equals(protectee)) {
+                        player.setProtected(true);
+                        poison = true;
+                    }
+                }
+                if(!poison) System.out.println("Choose a valid target.");
+            } while(poison == false);
+        }
     }
 }
